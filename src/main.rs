145db1ae81bc9c9c916a78a853912311efdff7e3
Gwylim Ashley <gwylim.a@gmail.com>
2025-10-20 14:33:26 -0700

    test

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,6 @@ fn main() {
     let mut input = String::new();
     std::io::stdin().lock().read_to_string(&mut input).unwrap();
     let bpe = cl100k_base().unwrap();
-    let tokens = bpe.encode_with_special_tokens(&input);
+    let tokens = bpe    .encode_with_special_tokens(&input);
     println!("{}", tokens.len());
 }
